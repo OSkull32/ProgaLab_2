d7f2b88b4b2cf4a33cd06a8c476f37e9aff84ade
Владимир Данченко <368087@niuitmo.ru>
2022-10-27 15:36:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import ru.ifmo.se.pokemon.*;
 public class Main{
     public static void main(String[] args){
         Battle b = new Battle();
-        Tropius p1 = new Tropius("Олег", 3);
-        Grimer p2 = new Grimer("Кирилл", 1);
-        Muk p3 = new Muk("Денис", 2);
-        Oddish p4 = new Oddish("Victor", 1);
-        Gloom p5 = new Gloom("Evgeniy", 2);
-        Vileplume p6 = new Vileplume("Anton", 3);
+        Tropius p1 = new Tropius("Andrey", 50);
+        Grimer p2 = new Grimer("Alex", 50);
+        Muk p3 = new Muk("Egor", 50);
+        Oddish p4 = new Oddish("Victor", 50);
+        Gloom p5 = new Gloom("Yaroslav", 50);
+        Vileplume p6 = new Vileplume("Vladimir", 50);
         b.addAlly(p1);
         b.addAlly(p2);
         b.addAlly(p3);
