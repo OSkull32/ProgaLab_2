4a73e1343aa182b5aac1e8142646ba57ba87510d
Владимир Данченко <368087@niuitmo.ru>
2022-10-27 15:32:32 +0300

    Update BulletSeed.java

--- src/moves/physical/BulletSeed.java
+++ src/moves/physical/BulletSeed.java
@@ -4,28 +4,21 @@ import ru.ifmo.se.pokemon.*;
 
 
 public class BulletSeed extends PhysicalMove {
-    int hits = 1;
+    public static int hits = 0;
     public BulletSeed() {
         super(Type.GRASS, 25, 100);
     }
     @Override
     protected double calcRandomDamage(Pokemon pokemon, Pokemon pokemon1) {
         double result = super.calcRandomDamage(pokemon, pokemon1);
-        if((this.power < 100 && Math.random()*8 < 3) || (this.power < 125 && Math.random()*8 < 1)) {
+        if((hits < 5) && (this.power < 100 && Math.random()*8 < 3) || (this.power < 125 && Math.random()*8 < 1)) {
             this.power += 25;
             hits++;
             attack(pokemon, pokemon1);
         }
         return result;
     }
-    @Override
-    protected void applySelfEffects(Pokemon pokemon) {
-        if(hits > 0) {
-            if(hits == 1)
-                this.power = 25;
-            hits--;
-        }
-    }
+
     @Override
     protected String describe() {
         return "Использовал Bullet Seed";
